7e6bc35f47bb988fe1553471806a0293b78a2b4a
Bhumika Dhingra <51711116+bhumikadhingra@users.noreply.github.com>
2020-09-01 20:25:33 +0530

    Update jupyter-server.sh

--- jupyter-server.sh
+++ jupyter-server.sh
@@ -1,8 +1,8 @@
-sudo apt-get install python3-pip3
-python3 -m pip3 install ipykernel
+sudo apt-get install python3-pip
+python3 -m pip install ipykernel
 python3 -m ipykernel install --user
-sudo apt-get install python3-pip3
-sudo pip3 install jupyter
+sudo apt-get install python3-pip
+sudo pip install jupyter
 jupyter notebook --generate-config
 wget https://raw.githubusercontent.com/Datum-Guy/Machine-learning-Inception-Public/master/jupyter_notebook_config.py
 mv ~/jupyter_notebook_config.py ~/.jupyter/
